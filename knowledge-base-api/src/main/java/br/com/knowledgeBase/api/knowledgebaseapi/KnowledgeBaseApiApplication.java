ef48ba12024065a302797fad709a7931b15f962a
João Victor Fernandes <jvsfernandes924@gmail.com>
2021-01-14 08:09:51 -0300

    Add api base

---
+++ knowledge-base-api/src/main/java/br/com/knowledgeBase/api/knowledgebaseapi/KnowledgeBaseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.knowledgeBase.api.knowledgebaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeBaseApiApplication.class, args);
+	}
+
+}
---
+++ knowledge-base-api/src/test/java/br/com/knowledgeBase/api/knowledgebaseapi/KnowledgeBaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.knowledgeBase.api.knowledgebaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
