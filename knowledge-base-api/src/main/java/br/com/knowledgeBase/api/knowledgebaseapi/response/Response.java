466f01adb8957772e5f98ef58b5ffa36cdaf95d7
João Victor Fernandes <jvsfernandes924@gmail.com>
2021-01-14 10:24:50 -0300

    Add Response Class

---
+++ knowledge-base-api/src/main/java/br/com/knowledgeBase/api/knowledgebaseapi/response/Response.java
@@ -0,0 +1,34 @@
+package br.com.knowledgeBase.api.knowledgebaseapi.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Used to encapsulate the return of the api
+public class Response<T> {
+
+    private T data;
+    private List<String> errors;
+
+    public Response() {
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<String> getErrors() {
+        if (this.errors == null) {
+            this.errors = new ArrayList<String>();
+        }
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+
+}
