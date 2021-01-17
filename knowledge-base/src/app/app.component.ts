1b6ee268d8644e11e8e42fa0846224a35f39f44f
João Victor Fernandes <jvsfernandes924@gmail.com>
2021-01-17 20:11:51 -0300

    Add frontend base

---
+++ knowledge-base/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kb-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  title = 'knowledge-base';
+}
