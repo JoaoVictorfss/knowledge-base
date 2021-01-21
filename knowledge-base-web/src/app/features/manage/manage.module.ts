597e455153d543cf1751750ec355e3aeeb46a11e
João Victor Fernandes <jvsfernandes924@gmail.com>
2021-01-20 21:56:00 -0300

    Update management page

--- knowledge-base-web/src/app/features/manage/manage.module.ts
+++ knowledge-base-web/src/app/features/manage/manage.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageComponent } from './manage.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ManageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
 })
 export class ManageModule { }
