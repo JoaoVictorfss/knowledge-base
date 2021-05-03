027d473e528344e4022fbe2e8526846511e41cc7
João Victor Fernandes <jvsfernandes924@gmail.com>
2021-05-03 17:48:56 -0300

    Code refactoring, add path constants

--- knowledge-base-api/src/main/java/br/com/knowledgeBase/api/knowledgebaseapi/Data/contants/PathConstants.java
+++ knowledge-base-api/src/main/java/br/com/knowledgeBase/api/knowledgebaseapi/Data/contants/PathConstants.java
@@ -1,6 +1,5 @@
 package br.com.knowledgeBase.api.knowledgebaseapi.Data.contants;
 
-
 public final class PathConstants {
     public static final String API_PATH = "/knowledgeBase-api";
     public static final String CATEGORY_ID_PARAM = "categoryId";
@@ -13,14 +12,14 @@ public final class PathConstants {
     public static final String CREATE = "/create";
     public static final String UPDATE = "/update" + "/{" + ID_PARAM + "}";
     public static final String DELETE = "/delete" + "/{" + ID_PARAM + "}";
-
+    
     public static final String ARTICLE_PATH = API_PATH + "/articles";
     public static final String LIST_BY_CATEGORY = "/list-by-category" + "/{" + CATEGORY_ID_PARAM + "}";
     public static final String PRIVATE_LIST_BY_CATEGORY = "/list/category" + "/{" + CATEGORY_ID_PARAM + "}";
     public static final String LIST_BY_SECTION = "/list-by-section" + "/{" + SECTION_ID_PARAM + "}";
     public static final String PRIVATE_LIST_BY_SECTION = "/list/section" + "/{" + SECTION_ID_PARAM + "}";
     public static final String FIND_BY_ARTICLE_ID = "/article" + "/{" + ID_PARAM + "}";
-
+    
     public static final String CATEGORY_PATH = API_PATH + "/categories";
     public static final String FIND_CATEGORY_BY_ID =  "/category" + "/{" + ID_PARAM + "}";
     
